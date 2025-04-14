b5bdea784c55355db0edd2770e33d249eb663b27
PC <ibrahimdrame165@gmail.com>
2025-04-14 18:27:37 +0200

    Arborescence + navbar et debut de la page d'accueil

---
+++ include/navbar.php
@@ -0,0 +1,29 @@
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/accueiletudiant.css">
+    <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+     <a href="#"><img src="../image/gustavedetouré.png" alt="Logo"  style="max-height: 40px;"></a>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+        <li class="nav-item px-5">
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item px-5">
+          <a class="nav-link" href="#">Planning</a>
+        </li>
+        <li class="nav-item dropdown px-5">
+          <a class="nav-link " href="#" role="button" >
+            Réservation
+          </a>
+        </li>
+        <li class="nav-item px-5 ">
+          <a  href="#" class="nav-link " aria-disabled="true">Contact</a>
+        </li>
+      </ul>
+      <a href="#"><img src="./image/pp.jpg" " style="max-height: 40px;" alt=""></a>
+    </div>
+  </div>
+</nav>
+    </header>
+    
---
+++ php/index.php
@@ -0,0 +1,37 @@
+<?php include "../include/navbar.php"   ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/accueiletudiant.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid  en-tete">
+        <h1 class="col-6  m-auto p-5 display-3 fw-bold text-light police-perso" style="text-align: center; "> Le matériel qu'il vous faut quand il vous le faut</h1>
+        <p class="text-center text-white fs-4 opacité">Gagnez du temps : tout le matériel de l’IUT accessible en ligne, en quelques clics.</p>
+        <div class="text-center p-5">
+            <button type="button  " class="btn btn-primary btn-lg mx-5 ">Réserver</button>
+            <button type="button" class="btn btn-secondary btn-lg mx-5 boutons">Nous contacter</button>
+        </div>
+    </div>
+
+    <div class="container-fluid container2">
+        <h2 class="col-6  m-auto p-5  fw-bold text-dark police-perso" style="text-align: center; "> Les avantages de notre service</h2>
+        <div class="text-center p-5">
+            <img src="../image/Salle138.JPG" class=" mx-3 custom-width " alt=" salle 138">
+            <img src="../image/Salle212.jpg" class=" mx-3 custom-width " alt="salle212">
+            <button type="button" class="btn btn-primary btn-lg d-block mx-auto mt-5">Réserver une salle</button>
+        </div>
+
+    </div>
+
+
+
+</body>
+
+</html>
