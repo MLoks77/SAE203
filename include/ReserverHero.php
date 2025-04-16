a1c4f8151db4de3b21582d908828ddf9f1481409
Ibrahim1655 <ibrahimdrame165@gmail.com>
2025-04-16 18:56:00 +0200

    ajout page réutilisable plusieurs fois pour qu'on puise les include et éviter redondance

---
+++ include/ReserverHero.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="../css/ReserverHero.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+<div class="header-banner">
+    <div class="header-text">Réserver</div>
+</div>
+<div class="fil_ariane container-fluid px-0">
+    <a href="#" class=" text-dark text-decoration-none ps-4">Accueil</a> &gt; <a href="#" class="text-dark text-decoration-none">Réserver</a><hr>
+</div>
