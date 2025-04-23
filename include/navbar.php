113d060b1923c8ee1826fa4794b8850d2ea1c78d
MDax <maximederenes@gmail.com>
2025-04-23 19:39:48 +0200

    Correction du css navbar.php
    Correction du css navbar.php
    Il y avait des erreurs dans le code

--- include/navbar.php
+++ include/navbar.php
@@ -1,5 +1,5 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/accueiletudiant.css">
+    <link rel="stylesheet" href="../css/accueiletudiant.css">
     <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
