d073c0d3e1e0b0f91f94a57f2a5c4d0a3392abd0
MLoks77 <maximederenes@gmail.com>
2025-05-20 12:36:54 +0200

    Correction pp navbar, réserver, La navbar dropdown ne fonctionne pas dans Réserver et Compte

--- include/navbaradmin.php
+++ include/navbaradmin.php
@@ -1,44 +1,52 @@
-
-<link rel="stylesheet" href="../css/accueiletudiant.css">
-
+<head>
+<link rel="stylesheet" href="../css/style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
 <header>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-     <a href=""><img src="../image/gustavedetouré.png" alt="Logo" style="max-height: 40px;"></a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item px-5" style="border-right: 1px solid black;">
-          <a class="nav-link" href="#">Admin</a>
-        </li>
-        <li class="nav-item px-5">
-          <a class="nav-link active" aria-current="page" href="../php/accueil.php">Accueil</a>
-        </li>
-        <li class="nav-item px-5">
-          <a class="nav-link" href="../php/planning.php">Planning</a>
-        </li>
-        <li class="nav-item dropdown px-5">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">
-            Réservation
-          </a>
-          <ul class="dropdown-menu"> <!-- Le dropdown bug, apparament c'est à cause du javascript -->
-            <li><a class="dropdown-item" href="#">Audiovisuel</a></li>
-            <li><a class="dropdown-item" href="#">Média</a></li>
-            <li><a class="dropdown-item" href="#">Casque</a></li>
-            <li><a class="dropdown-item" href="#">Salle</a></li>
-          </ul>
-        </li>    
-        <li class="nav-item px-5">
-          <a href="../php/contact.php" class="nav-link" aria-disabled="true">Contact</a>
-        </li>
-      </ul>
-      <a href="" role="button" data-bs-toggle="dropdown"><img src="../image/pp.jpg" style="max-height: 40px;" alt="photo de profil"></a>
-      <ul class="dropdown-menu"> <!-- Le dropdown bug, apparament c'est à cause du javascript -->
-            <li><a class="dropdown-item" href="#">Paramètres</a></li> <!-- Lien vers la page paramètre à mettre -->
-            <li><a class="dropdown-item" href="../configdb/delete-session.php">Se déconnecter</a></li> <!-- mettre un clear session dans delete-session.php et faire en sorte qu'il renvoie vers connexion -->
-          </ul>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+
+      <a class="navbar-brand" href="#">
+        <img src="../image/gustavedetouré.png" alt="Logo" style="max-height: 40px;">
+      </a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0 align-items-center">
+          <li class="nav-item px-3" style="border-right: 1px solid black;">
+            <a class="nav-link fw-bold" href="#">Admin</a>
+          </li>
+          <li class="nav-item px-3">
+            <a class="nav-link" href="../php/accueil.php">Accueil</a>
+          </li>
+          <li class="nav-item px-3">
+            <a class="nav-link" href="../php/planning.php">Planning</a>
+          </li>
+          <li class="nav-item px-3">
+            <a class="nav-link" href="../php/Reserver.php" id="reservation" role="button">
+              Réservation
+            </a>
+          </li>
+          <li class="nav-item px-3">
+            <a class="nav-link" href="../php/contact.php">Contact</a>
+          </li>
+          <li class="nav-item dropdown px-3">
+            <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="../image/pp.jpg" alt="photo de profil" style="max-height: 40px; border-radius: 50%;">
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="../php/compte.php">Paramètres</a></li>
+              <li><a class="dropdown-item" href="../configdb/logout.php">Se déconnecter</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </header>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldnv0O8r+Lk5C6mZ5p2eD5z4z5j5z5j5j5j5j5j5" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
+</body>
